e8e98abb34bdf2b9c73182c4f97cf1b1f9f71a8b
hacker65536 <s.hacker65536@gmail.com>
2017-05-30 02:25:07 +0000

    Update

--- setup.sh
+++ setup.sh
@@ -9,12 +9,12 @@ cat Dockerfile
 
 cat <<'EOF' > Dockerfile
 FROM scratch
-ARG PACKER_VERSION=0.12.3
-ARG TERRAFORM_VERSION=0.9.5
-ARG ANSIBLE_VERSION=2.2.2.0
+ARG PACKER_VERSION
+ARG TERRAFORM_VERSION
+ARG ANSIBLE_VERSION
 ADD amzn-container-minimal-2017.03.0.20170401-x86_64.tar.xz /
 RUN yum install -y jq python27-devel openssl-devel openssh-clients libffi-devel gcc unzip; yum clean all
-RUN curl -s -O https://bootstrap.pypa.io/get-pip.py && python get-pip.py && rm get-pip.py && pip install ansible && pip install awscli
+RUN curl -s -O https://bootstrap.pypa.io/get-pip.py && python get-pip.py && rm get-pip.py && pip install ansible==${ANSIBLE_VERSION} && pip install awscli
 RUN curl -s -O https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip && \
     unzip packer_${PACKER_VERSION}_linux_amd64.zip && \
     mv ./packer /usr/local/bin/ &&  rm ./packer_${PACKER_VERSION}_linux_amd64.zip 
